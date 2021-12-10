18612439b238f879625fecb625380c4f2bff3dee
fearm3 <ibraa1628@gmail.com>
2021-12-10 15:58:44 +0300

    first commit

---
+++ language-cards/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+import Cards from "./components/cards/Cards";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
